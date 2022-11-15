81e41428da96faa9a3ec86685a005ec25746aa65
Prince whyte dabotubo <princewhyte02@gmail.com>
2022-11-15 17:13:05 +0100

    feat:index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Link from "@mui/material/Link"
+import Typography from "@mui/material/Typography"
+
 export default function Home() {
-  return <div>Index page</div>
+  return (
+    <div>
+      <Link href="/auth/login" underline="none">
+        <Typography sx={{ color: "#1B1B1B", fontSize: { xs: "0.815rem" } }}>Login</Typography>
+      </Link>
+    </div>
+  )
 }
